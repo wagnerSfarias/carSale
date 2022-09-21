6ca2fee44fae4a0aaa76635660ff1d915edb2c5d
wagnerSfarias <wagnner@universalhost.com.br>
2022-09-21 20:05:35 -0300

    insert imagens

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const api = axios.create({
     // baseURL:'http://192.168.0.101:1337/'
-    // baseURL:'http://192.168.0.101:3000'
-    baseURL:'https://api-salecar.herokuapp.com/'
+    baseURL:'http://192.168.0.101:3000'
+    // baseURL:'https://api-salecar.herokuapp.com/'
     
 })
 
